04f9a0bb11f6d4e03272adad3e30a46166cc8ab5
layerbag <gabriel201801498@gmail.com>
2021-05-16 16:49:07 -0300

    alterações

--- leitor/leitor.c
+++ leitor/leitor.c
@@ -18,14 +18,20 @@ void remove_newline(char *ptr)
 }
 
 typedef struct var{
-  int x;
-  int * v;
+  char str_var[10];
+  int posicao;
 }variavel;
 
-typedef struct parametros{
-  int rdi,rsi,rdx;
-}parametro;
+typedef struct funcoes{
+  int tam_pilha,qtd_var;
+  char str[LINESZ];
+  char *op;
+  variavel *pilha;
+}funcao;
 
+char* operacoes(char*line, funcao f){
+  
+}
 int main()
 {
   char v1;
@@ -34,6 +40,7 @@ int main()
   int inicio, fim, passo;
   char line[LINESZ],temp[LINESZ];
   int count = 0, bloco = 0;
+  funcao f1;
   
   // Lê uma linha por vez
   while (fgets(line, LINESZ, stdin) != NULL) {
@@ -42,8 +49,16 @@ int main()
 
     // Verifica se line começa com 'end' (3 letras)
     if (strncmp(line, "end", 6) == 0) {
-      printf("leave\nret\n");
-	    
+      if(f1.tam_pilha != 0){
+        if(f1.tam_pilha % 16 != 0){
+          i1 = f1.tam_pilha % 16;
+          f1.tam_pilha += i1;
+        }
+        sprintf(temp, "  subq $%d, %%rsp\n", f1.tam_pilha);
+        strcat(f1.str, temp);
+      }
+      printf("%s",f1.str);
+      printf("leave\nret\n\n");
       continue;
     }
 
@@ -51,27 +66,10 @@ int main()
     // verifica função
     r = sscanf(line, "function f%c p%c1, p%c2, p%c3", &f, &p1, &p2, &p3);
     
-    if (r == 4) {
+    if (r >= 2) {
       printf(".globl f%c\nf%c:\n  pushq %%rbp\n  movq %%rsp, %%rbp\n",f,f);
-      /*printf("Linha %d: %s\n", count, line);
-      printf("Lido 'f%c' 'p%c1' 'p%c2' 'p%c3'\n",f,  p1, p2, p3);
-      printf("---\n");*/
-      continue;
-    }
-    if (r == 3) {
-      printf(".globl f%c\nf%c:\n  pushq %%rbp\n  movq %%rsp, %%rbp\n",f,f);
-      /*printf("Linha %d: %s\n", count, line);
-      printf("Lido 'f%c' 'p%c1' 'p%c2'\n",f, p1, p2) ;
-      printf("---\n");*/
-      continue;
-    }
-    if (r == 2) {
-      printf(".globl f%c\nf%c:\n  pushq %%rbp\n  movq %%rsp, %%rbp\n",f,f);
-      
-      /*printf("Linha %d: %s\n", count, line);
-      printf("Lido 'f%c' 'p%c1'\n",f, p1) ;
-      printf("---\n");*/
-
+      f1.tam_pilha = 0;
+      f1.qtd_var = 0;
       continue;
     }
     
@@ -94,67 +92,67 @@ int main()
     if(bloco == 1){
       // se for um int
       if(strncmp(line,"var",3) == 0){
-      r = sscanf(line,"var vi%d", &i1);
-        printf("linha %d: %s\n", count, line);
-        printf("Lido 'vi%d'\n",i1);
+        r = sscanf(line,"var vi%d", &i1);     // pega o numero da variavel
+        f1.qtd_var++;                         // inc a quantidade de variaveis da funcao
+        f1.tam_pilha += 4;                    // soma +4 no tamanho da pilha
+        
+        if(f1.qtd_var == 1){
+          f1.pilha = malloc(sizeof(variavel));  // se for o primeiro elemento faz o malloc
+        }else{
+        //  realloc(f1.pilha,f1.qtd_var * sizeof(variavel));  // senão faz o realloc
+        }
+
+        f1.pilha[i1].posicao = f1.tam_pilha * -1;    // posição do elemento adicionado na pilha
+
       }
 
       // se for um array de int
       if(strncmp(line,"vet",3)==0){
-       r = sscanf(line,"vet va%d size ci%d", &i1,&i2);
-        printf("linha %d: %s\n", count, line);
-        printf("Lido 'va%d' size: '%d'\n",i1,i2);
+        r = sscanf(line,"vet va%d size ci%d", &i1,&i2);
+        f1.qtd_var++;
+        f1.tam_pilha += i2*4;
+
+        if(f1.qtd_var == 1){
+          f1.pilha = malloc(sizeof(variavel));  // se for o primeiro elemento faz o malloc
+        }else{
+        //  realloc(f1.pilha,f1.qtd_var * sizeof(variavel));  // senão faz o realloc
+        }
+
+        f1.pilha[i1].posicao = f1.tam_pilha * -1;
       }
 
       // finaliza o bloco
       if(strncmp(line,"enddef",6)==0){
         bloco = 0;
       }
-    }else{    //Operações com as variaveis
-      if(line[1] == 'i' || line[1] == 'a'){
-        char*t;
-        printf("linha %d: %s\n",count, line);
-        t = strtok(line," ");
-        i1 = 0;
+    }else if(line[1] == 'i' || line[1] == 'a'){  //Operações com as variaveis
 
-        while(t != NULL){
+      //strcat(f1.op, operacoes(line, f1));
+        
+    }else if(strncmp(line,"return p",8) != 0){
+      sscanf(line,"return %d",&i1);
+      printf("  movl $%d, %%eax\n",i1);
+      continue;
 
-          if(t[0] == '+'){
-            printf("+");
-          }else if(t[0] == '-'){
-            printf("-");
-          }else if(t[0] == '*'){
-            printf("*");
-          }else if(t[0] != '='){
-            printf("%s",t);
-          }
+    }else if(strncmp(line, "return p",8) == 0){
 
-          t = strtok(NULL," ");
-    
-        }
-        printf("\n");
-      }else if(strncmp(line,"return",6) == 0){
-        if(strncmp(line,"return p",8) != 0){
-          sscanf(line,"return %d",&i1);
-          printf("  movl $%d, %%eax\n",i1);
-        }
-
-        sscanf(line,"return p%c%d",&p1,&i1);
-        if(i1 == 1) {
-          printf("  movl %%edi, %%eax\n");
-          continue;
-          }
-        if(i1 == 2) {
-          printf("  movl %%esi, %%eax\n");
-          continue;
-          }
-        if(i1 == 3) {
-          printf("  movl %%edx, %%eax\n");
-          continue;
-          }
+      sscanf(line,"return p%c%d",&p1,&i1);
+      if(i1 == 1) {
+        printf("  movl %%edi, %%eax\n");
+        continue;
       }
-    }
-
+      if(i1 == 2) {
+        printf("  movl %%esi, %%eax\n");
+        continue;
+      }
+      if(i1 == 3) {
+        printf("  movl %%edx, %%eax\n");
+        continue;
+      }
+    }else if(strncmp(line, "return v", 8) == 0){
+     // sscanf(line, "return v%*c%d", i1);
+     // sprintf(temp, "movl %d(%%rbp), %eax", f1.pilha[i1].posicao);
+    }  
 
   }
 
